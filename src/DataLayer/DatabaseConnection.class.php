b80be21ce664c043f9330e69d36bf108c20a9842
bfj5889 <bfj5889@g.rit.edu>
2018-04-06 13:59:09 -0400

    Database Connection Rework

--- src/DataLayer/DatabaseConnection.class.php
+++ src/DataLayer/DatabaseConnection.class.php
@@ -1,27 +1,98 @@
 <?php
 require_once 'dbconfig.php';
-
 /**
  * Class: DatabaseConnection
  * Date: 4/6/2018
  * Description:
  */
+
 class DatabaseConnection {
-  public function __construct() {
-    $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+
+  private $_connection;
+
+  private static $_instance; //The single instance
+
+
+  /**
+   * Retrieves and instance of the Database. If an instance is already
+   * made it returns the same instance. If there is no previous instance
+   * then a new instance is created.
+   * @return DatabaseConnection
+   */
+  public static function getInstance() {
+    if (!self ::$_instance) { // If no instance then make one
+      self ::$_instance = new self();
+    }
+    return self ::$_instance;
+  }
+
+
+  /**
+   * DatabaseConnection constructor.
+   */
+  private function __construct() {
+    global $host;
+    global $port;
+    global $dbname;
+    global $username;
+    global $password;
 
     try {
-      // create a PostgreSQL database connection
-      $conn = new PDO($dsn);
+      $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$username;password=$password";
+      $this -> _connection = new PDO($dsn);
 
-      // display a message if connected to the PostgreSQL successfully
-      if ($conn) {
-        echo "Connected to the <strong>$db</strong> database successfully!";
+      if ($this -> $_connection) {
+        echo "Connected to the <strong>$this -> $_connection</strong> database successfully!";
       }
-    }catch
-    (PDOException $e){
-      // report error message
+
+    } catch (PDOException $e) {
+      #Open log file and add error message
+      echo $e -> getMessage() . "\n";
+      $logFile = 'phpErrors.txt';
+      $currentLogFile = file_get_contents($logFile);
+      $currentLogFile .= "\n" . date('l jS \of F Y h:i:s A') . $e -> getMessage();
+      file_put_contents($logFile, $currentLogFile);
+      exit();
+    }
+  }
+
+
+  /**
+   * Retrieves an instance of the Database connection, not the instance.
+   * @return PDO
+   */
+  function getConnection() {
+    $dbh = null;
+    return $this -> _connection;
+  }
+
+
+  // Magic method clone is empty to prevent duplication of connection
+  private function __clone() {
+  }
+
+
+  /**
+   * @param $objName - Name of Object / Database Table
+   * @param string $sqlString - Complete sql select statement
+   * @return array - An associative array of objects pulled from the database
+   */
+  function returnObject($objName, $sqlString = "") {
+    try {
+      $results = array();
+      if ($sqlString == "") {
+        $sqlString = "SELECT * FROM " . $objName;
+      }
+      $stmnt = $this -> getConnection() -> prepare($sqlString);
+      $stmnt -> execute();
+      $stmnt -> setFetchMode(PDO::FETCH_CLASS, $objName);
+      while ($result = $stmnt -> fetch()) { // or just fetchALl();
+        $results[] = $result;
+      }
+      return $results;
+    } catch (PDOException $e) {
       echo $e -> getMessage();
+      die();
     }
   }
 }
