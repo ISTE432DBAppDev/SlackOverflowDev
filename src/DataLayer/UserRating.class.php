1f472509461ec4c5253215e238c2e13ce8d2d12c
Brianna Jones <bfj5889@g.rit.edu>
2018-04-06 15:36:10 -0400

    Filling in create and check user rating

--- src/DataLayer/UserRating.class.php
+++ src/DataLayer/UserRating.class.php
@@ -38,10 +38,33 @@ class UserRating {
   }
 
   public function createUserRating($accountID, $tipID){
+    try {
+      $dbconn = $this -> getDBInfo();
+      $result = pg_prepare($dbconn, "createUserRatingQuery", "INSERT INTO USERRATING (accountID, tipsID) VALUES ($1, $2)");
+      $result = pg_execute($dbconn, "createUserRatingQuery", array($accountID, $tipID );
+  return $result;
+    } catch (Exception $e)  {
+    echo $e;
     return null;
+    }
   }
 
   public function checkUserRating($accountID, $tipID){
+    try {
+      $dbconn = $this -> getDBInfo();
+      $result = pg_prepare($dbconn, "checkUserRatingQuery", "SELECT FROM USERRATING WHERE accountID = $1 AND tipID = $2");
+      $result = pg_execute($dbconn, "checkUserRatingQuery", array($accountID, $tipID );
+      
+      $numRows = pg_num_rows($result);
+      if ($count == 1) {
+        return true;
+      } else {
+        return false;
+      }
+  return $result;
+    } catch (Exception $e)  {
+    echo $e;
     return null;
+    }
   }
 }
