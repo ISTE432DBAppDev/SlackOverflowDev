73b1d705458d40099f29328cdba90f4a5ae28386
Brianna Jones <bfj5889@g.rit.edu>
2018-04-06 15:26:41 -0400

    Filling in create and login functions

--- src/DataLayer/AccountData.class.php
+++ src/DataLayer/AccountData.class.php
@@ -1,4 +1,5 @@
 <?php
+include 'DatabaseConnection.class.php';
 /**
  * Class: AccountData
  * Date: 4/4/2018
@@ -37,17 +38,45 @@ class AccountData {
     $this -> userName = $userName;
   }
 
-
-
+  private function getDBInfo() {
+     try {
+       $instance = DatabaseConnection ::getInstance();
+       return $conn = $instance -> getConnection();
+     } catch (Exception $e) {
+       echo $e -> getMessage();
+       return null;
+     }
+  }
+  
   public function createAccount($userName, $pwd){
+    try {
+      $dbconn = $this -> getDBInfo();
+      $result = pg_prepare($dbconn, "createAccountQuery", "INSERT INTO ACCOUNTS (username, password) VALUES ($1, $2)");
+      $result = pg_execute($dbconn, "createAccountQuery", array($userName, SHA1($pwd)) );
+  return $result;
+    } catch (Exception $e)  {
+    echo $e;
     return null;
+    }
+    
   }
 
   public function loginAccount($userName, $pwd) {
+    try {
+      $dbconn = $this -> getDBInfo();
+      $result = pg_prepare($dbconn, "loginAccountQuery", "SELECT accountID FROM ACCOUNTS WHERE username=$1 AND password=$2");
+      $result = pg_execute($dbconn, "loginAccountQuery", array($userName, SHA1($pwd)) );
+      
+      $numRows = pg_num_rows($result);
+      if ($count == 1) {
+        return true;
+      } else {
+        return false;
+      }
+  
+    } catch (Exception $e)  {
+    echo $e;
     return null;
-  }
-
-  public function logoutAccount(){
-    return null;
+    }
   }
 }
